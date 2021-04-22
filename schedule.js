917e5dc10ef46450c1ab11996e3d351e4c3b396c
Mike S <mschoenike1@my.wctc.edu>
2021-04-21 22:37:07 -0500

    added functions

--- schedule.js
+++ schedule.js
@@ -4,5 +4,49 @@ $(document).ready(function() {
 
 function selectSchedule(event) {
     event.preventDefault();
+    let events = "";
+    let times = "";
+    let day = $("input[name=days]:checked").val();
+
+    switch(day)
+    {
+        case "Sunday":
+            events = "Sleep in";
+            times = "Whenever";
+            break;
+
+        case "Monday":
+            events = "Curse at clock";
+            times = "7 am";
+            break;
+
+        case "Tuesday":
+            events = "Watch TV";
+            times = "8 pm";
+            break;
+
+        case "Wednesday":
+            events = "Javascript Class";
+            times = "6 pm";
+            break;
+
+        case "Thursday":
+            events = "Date night with the Wife";
+            times = "6:30 pm";
+            break;
+
+        case "Friday":
+            events = "Leave Work Early";
+            times = "1 pm";
+            break;
+
+        case "Saturday":
+            events = "Watch Football";
+            times = "All Day";
+            break;
+    }
+
+    $("p#events").text(events);
+    $("p#times").text(times);
 
 }
